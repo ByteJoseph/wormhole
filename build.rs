aa031832a3be4b57fb8f9cf6ad4d2fada6b7fe15
ByteJoseph <josephnjoseph43@gmail.com>
2026-02-28 16:53:14 +0530

    refactor main into simple functions
    refactor main into simple functions

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // cc::Build::new()
-    //     .cpp(true)
-    //     .file("cpp/main.cpp")
-    //     .include("include")
-    //     .flag_if_supported("-std=c++17")
-    //     .compile("cpp_main");
-}
